96cadb2b848a842a8c4cc1ceccf700ae5f7412ea
Development <andrewkornes@gmail.com>
2020-03-22 18:21:57 -0400

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {PostCreateComponent} from  './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } "@angular/material/input";
 @NgModule({
   declarations: [
     AppComponent,
@@ -10,7 +12,9 @@ import {PostCreateComponent} from  './posts/post-create/post-create.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
